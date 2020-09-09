c58b3a617ada645cb74ca00a9103a3eb259f58d7
chenhualihx <39735751+chenhualihx@users.noreply.github.com>
2020-09-09 17:12:36 -0400

    Create patch_index.tsx
    I added line 44-47. after add useEffect, we can set checked/unchecked programmally.

---
+++ src/components/CheckRow/patch_index.tsx
@@ -0,0 +1,62 @@
+// Dependencies
+import React from 'react';
+import { CheckBox, CheckBoxProps } from 'react-native-elements';
+
+// Components
+import BaseRow, { BaseRowProps } from '../BaseRow';
+import { useEffect } from 'react';
+
+/**
+ * @interface CheckRowProps
+ * @description The properties definition of check row.
+ */
+export interface CheckRowProps extends BaseRowProps {
+  /**
+   * Flag to checking the input.
+   * @default false
+   */
+  checked: boolean;
+
+  /**
+   * Callback to communicate when the value changed.
+   */
+  onValueChange?: (isChecked: boolean) => void;
+
+  /**
+   * The properties pass to checkbox.
+   */
+  checkboxProps: CheckBoxProps;
+}
+
+/**
+ * @component CheckRow
+ * @author Daniel Mejia
+ * @description Row component for settings list with a checkbox.
+ */
+export function CheckRow(props: CheckRowProps): React.ReactElement {
+  const { checked, onValueChange, checkboxProps } = props;
+  const [isChecked, setChecked] = React.useState(checked);
+  const onChange = (): void => {
+    setChecked(!isChecked);
+    onValueChange?.(!isChecked);
+  };
+
+  useEffect(()=>{
+     setChecked(checked)
+  }, [checked]);
+
+  return (
+    <BaseRow
+      {...props}
+      rightContent={(
+        <CheckBox
+          {...checkboxProps}
+          checked={isChecked}
+          onPress={onChange}
+        />
+      )}
+    />
+  );
+}
+
+export default CheckRow;
